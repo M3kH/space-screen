8a616fce7c2b0df204c68ee6638c6aa89d94c273
Mauro <mauro@insided.nl>
2016-09-17 13:41:20 +0200

    Add profile functionalities

--- bin/index.js
+++ bin/index.js
@@ -6,13 +6,21 @@ var fs = require('fs');
 var path = require('path');
 var homePath = process.env[(process.platform=='win32') ? 'USERPROFILE' : 'HOME'];
 
+function hasProfile( json ){
+  if(!json.profiles) return json;
+  var profile = process.argv[3] || Object.keys(json.profiles)[0];
+  console.log(Object.keys(json.profiles));
+  return json[profile];
+}
+
 
 (function(){
   var filePath = !configFile ? path.resolve(homePath, '.space-screen') : path.resolve(process.env.PWD, configFile);
 
   try{
-    var json = JSON.parse(fs.readFileSync(filePath, { encoding: 'utf8' }));
+    var json = hasProfile(JSON.parse(fs.readFileSync(filePath, { encoding: 'utf8' })));
   }catch(e){
+    console.log(e);
     console.log("We couldn't find the right configuration file, we were looking for: \n"+filePath);
     return false;
   }
